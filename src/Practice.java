05d90e45eabc5f34bd00e2de8c01719dac2c87d6
pdunmire-pcd <203000868+pdunmire-pcd@users.noreply.github.com>
2026-09-23 20:24:32 -0700

    Fix frequencies import, implement levelCount

--- src/Practice.java
+++ src/Practice.java
@@ -135,7 +135,13 @@ public class Practice {
      * @return the number of levels in the tree
      */
     public static int levelCount(BinaryTreeNode<?> root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+        
+        int leftLevels = levelCount(root.left);
+        int rightLevels = levelCount(root.right);
+        return Math.max(leftLevels, rightLevels) + 1;
     }
 
 
