97f1134e03523ef94d7b7c08b443924f4cc08c02
pdunmire-pcd <203000868+pdunmire-pcd@users.noreply.github.com>
2026-09-23 20:15:05 -0700

    implemented shortestWord

--- src/Practice.java
+++ src/Practice.java
@@ -37,7 +37,21 @@ public class Practice {
      * @throws NullPointerException if words is null
      */
     public static String shortestWord(Set<String> words) {
-        return null;
+        if (words == null) {
+            throw new NullPointerException();
+        }
+        if (words.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        
+        String shortest = null;
+        for (String word : words) {
+            if (shortest == null || word.length() < shortest.length() || 
+                (word.length() == shortest.length() && word.compareTo(shortest) < 0)) {
+                shortest = word;
+            }
+        }
+        return shortest;
     }
 
     /**
